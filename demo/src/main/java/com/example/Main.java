9637cf62b91645165a8abd4f426e5437ba4ab5d7
TomasJaures <t.jaures01@ufromail.cl>
2026-06-27 22:16:12 -0400

    Salida implementada y formateada

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -34,7 +34,7 @@ public class Main {
         );
 
         int dp[][] = createTableDP(items, cap);
-        
+        createReport(items, cap, dp);
     }
 
     public static int[][] createTableDP(List<Item> items, int cap){
@@ -59,11 +59,75 @@ public class Main {
         return dp;
     }
 
+     public static void createReport(List<Item> items, int cap, int[][] dp) {
+        int n = items.size();
+
+        //Agregar a la mochila
+        int x = n;
+        int y = cap;
+        List<Item> bp = new ArrayList<>();
+
+        while (x > 0 && y > 0) {
+            if (dp[x][y] == dp[x - 1][y]) {
+                //System.out.println("Descartado: " + items.get(x - 1).name);
+                x--;
+            } else {
+                //System.out.println("Seleccionado: " + items.get(x - 1).name);
+                bp.add(items.get(x - 1));
+                y -= items.get(x - 1).weight;
+                x--;
+            }
+        }
+
+        int sumW = 0;
+        System.out.println("Items seleccionados: ");
+        for (Item item : bp) {
+            
+            sumW += item.weight;
+            System.out.println("- " + item.name);
+        }
+        System.out.println("\nPeso sumado: (" + sumW + " / " + cap + ")");
+        System.out.println("Valor maximo alcanzado: " + dp[n][cap]);
+
+        y = n;
+        x = cap;
+        //Formatear Table DP (toString)
+        
+        printTableDP(items, cap, dp);
+    }
    
 
-    public static void printTableDP(int dp[][]){
-        for (int i = 0; i < dp.length; i++) {
-            System.out.println(Arrays.toString(dp[i]));
+    public static void printTableDP(List<Item> items, int cap, int[][] dp){
+        System.out.println("==================================== TABLA DP =====================================");
+        int n = items.size() - 1;
+
+        //Linea "0 0 0 0 0 0 0 0 0 0 0 0 ... (Sin item)"
+        System.out.print(String.format("%-20s", "No item: "));
+        for (int i = 0; i < dp[0].length; i++) {
+            System.out.print(String.format("%-3s", dp[0][i]));
         }
+        System.out.println();
+
+        //Siguientes lineas:
+        for (int i = 1; i < dp.length; i++) {
+            System.out.print(String.format("%-20s", items.get(n - i + 1).name + ": "));
+            for (int j = 0; j < dp[i].length; j++) {
+                System.out.print(String.format("%-3s", dp[i][j]));
+            }
+            System.out.println();
+            //System.out.println(Arrays.toString(dp[i]));
+        }
+        System.out.print("===================================================================================");
     }
 }
+
+/*
+
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+[0, 0, 6, 8, 8, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14]
+[0, 0, 6, 8, 8, 14, 14, 15, 15, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21]
+[0, 0, 6, 8, 8, 14, 14, 16, 18, 21, 24, 24, 25, 25, 31, 31, 31, 31, 31, 31, 31]
+[0, 0, 6, 8, 8, 14, 14, 16, 18, 21, 24, 24, 25, 25, 31, 31, 31, 33, 36, 39, 39]
+[0, 0, 6, 8, 8, 14, 14, 16, 18, 21, 24, 24, 26, 26, 31, 31, 33, 36, 36, 39, 39]
+*/
